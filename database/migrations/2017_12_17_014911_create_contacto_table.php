8b01fc3ce21c5476759be82349db41c59fb388a2
lfmll <luisfernandomedinallorenti@gmail.com>
2017-12-19 11:20:21 -0400

    Add files via upload

---
+++ database/migrations/2017_12_17_014911_create_contacto_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nick');
+            $table->integer('celular');
+            $table->string('email');
+            $table->string('estado');
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
